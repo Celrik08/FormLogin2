f6841008c30000f49aed34b1043683ee7cbcc4b2
Celrik08 <m.fajar080907@gmail.com>
2024-07-08 14:45:06 +0800

    Add project files.

---
+++ FormLogin1/Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+
+    Private Sub TextUser_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextUser.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextPassword.Focus()
+        End If
+    End Sub
+    ' Tombol Login ditekan
+    Private Sub BtnLogin_Click(sender As Object, e As EventArgs) Handles BtnLogin.Click
+        PerformLogin()  ' Panggil fungsi untuk melakukan login
+    End Sub
+
+    Private Sub TextPassword_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextPassword.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            e.Handled = True
+            PerformLogin()
+        End If
+    End Sub
+
+    ' Fungsi untuk melakukan login
+    Private Sub PerformLogin()
+        Dim username As String = TextUser.Text.ToLower()
+        Dim password As String = TextPassword.Text
+
+        If CheckCredentials(username, password) Then
+            ' Tampilkan pesan login berhasil
+            MessageBox.Show("Login Berhasil.", "Selamat", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Dim fullName As String = GetFullName(username)
+            Dim FormPenjualan As New FormPenjualan(fullName)
+            FormPenjualan.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Login Gagal. Username atau Password salah.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            TextUser.Text = ""
+            TextPassword.Text = ""
+        End If
+    End Sub
+
+    Private Function CheckCredentials(username As String, password As String) As Boolean
+        Select Case username
+            Case "kasir0001"
+                Return password = "327469"
+            Case "kasir0002"
+                Return password = "469725"
+            Case "admin"
+                Return password = "admin00"
+            Case Else
+                Return False
+        End Select
+    End Function
+
+    Private Function GetFullName(username As String) As String
+        Select Case username
+            Case "kasir0001"
+                Return "Kartika"
+            Case "kasir0002"
+                Return "Ferdinan"
+            Case "admin"
+                Return "Admin"
+            Case Else
+                Return "Unknown"
+        End Select
+    End Function
+    Private Sub FormLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextPassword.PasswordChar = "*"
+    End Sub
+End Class
---
+++ FormLogin1/FormKasir.vb
@@ -0,0 +1,54 @@
+﻿Public Class FormKasir
+
+    Private Sub TextKode_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextKode.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextJumlah.Focus()
+        End If
+    End Sub
+    Private Sub TextKode_TextChanged(sender As Object, e As EventArgs) Handles TextKode.TextChanged
+        Dim kode As String = TextKode.Text
+
+        If kode = "1000" Then
+            TextNama.Text = "Shampo Sunslik"
+            TextHarga.Text = "Rp. 390.000"
+        ElseIf kode = "1001" Then
+            TextNama.Text = "Jazz1"
+            TextHarga.Text = "Rp. 221.000"
+        ElseIf kode = "1002" Then
+            TextNama.Text = "Downy Passion"
+            TextHarga.Text = "Rp. 280.500"
+        Else
+            TextNama.Text = ""
+            TextHarga.Text = ""
+        End If
+    End Sub
+
+    Private Sub TextJumlah_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextJumlah.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            CalculateSubTotal()
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub CalculateSubTotal()
+        Dim jumlah As Integer = Convert.ToInt32(TextJumlah.Text)
+        Dim harga As Decimal = Convert.ToDecimal(TextHarga.Text.Replace("Rp. ", "").Replace(".", ""))
+        Dim subTotal As Decimal = harga * jumlah
+        TextSub.Text = subTotal.ToString("C")
+    End Sub
+    Private Sub BtnSimpan_Click(sender As Object, e As EventArgs) Handles BtnSimpan.Click
+        Dim kode As String = TextKode.Text
+        Dim nama As String = TextNama.Text
+        Dim harga As String = TextHarga.Text
+        Dim jumlah As String = TextJumlah.Text
+        Dim subTotal As String = TextSub.Text
+
+        Dim formPenjualan As FormPenjualan = CType(Application.OpenForms("FormPenjualan"), FormPenjualan)
+        If formPenjualan IsNot Nothing Then
+            formPenjualan.AddDataToDataGridView(kode, nama, harga, jumlah, subTotal)
+            formPenjualan.UpdateTotal(subTotal) ' Update total pada FormPenjualan
+            formPenjualan.Show() ' Buka FormPenjualan
+            Me.Hide() ' Sembunyikan FormKasir
+        End If
+    End Sub
+End Class
